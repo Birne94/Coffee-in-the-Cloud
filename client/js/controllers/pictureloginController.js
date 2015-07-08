7af1f2f7793dc31e55cdc47d8f50f4a6f32f44ef
Daniel Birnstiel <daniel.birnstiel@student.hpi.de>
2015-07-08 12:50:29 +0200

    fixed spelling mistake on tablet mode

--- client/js/controllers/pictureloginController.js
+++ client/js/controllers/pictureloginController.js
@@ -23,7 +23,7 @@ define(["jquery"], function (jQuery) {
             $.each(data, function (idx, obj) {
                 if (today==obj.date){
                     $scope.cleaning=true;
-                    $scope.cleaning_message=obj.user.first_name + " " + obj.user.last_name +" have to do the"+ (obj.type == "w" ? " weekly " : (obj.type == "b" ? " biweekly " : " other "))+"cleaning today";
+                    $scope.cleaning_message=obj.user.first_name + " " + obj.user.last_name +" has to do the"+ (obj.type == "w" ? " weekly " : (obj.type == "b" ? " biweekly " : " other "))+"cleaning today";
                 }
             });
         });
